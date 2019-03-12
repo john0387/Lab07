a8d112718f46c86c4e2f8fb95c6b7159c2f3775f
Steven <Steven.A.Johnson-1@ou.edu>
2019-03-12 15:39:05 -0500

    second commit, all files created with respective skeletons, error implementing the hospital class

---
+++ PriorityQueueHospital.java
@@ -0,0 +1,29 @@
+
+public class PriorityQueueHospital implements Hospital{
+	
+	public PriorityQueueHospital() {
+		
+	}
+	public void addPatient(PatientType patient) {
+		
+	}
+	public PatientType nextPatient() {
+		
+	}
+	public PatientType treatNextPatient() {
+		
+	}
+	public int numPatients() {
+		
+	}
+	public String hospitalType() {
+		
+	}
+	public String allPatientInfo() {
+		
+	}
+	public String toString()
+	{
+		return String.format("A %s-type hospital with %d patients.", hospitalType(), numPatients());
+	}
+}
---
+++ Todo.java
@@ -0,0 +1,27 @@
+public class Todo {/*
+Lab Section_________: 011
+Last Modified_______: 2019-03-12
+OU ID_______________: 113484715
+OU EMAIL____________: Steven.A.Johnson-1@ou.edu
+
+Tasks:
+1. 
+2. 
+3.  
+4. 
+5. 
+
+Estimated Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+
+Actual Time (minutes):
+1. 
+2. 
+3. 
+4. 
+5. 
+*/}
